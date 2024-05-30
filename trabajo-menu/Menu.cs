651aa8ed017147bfb364428a6b81392fa0a85d53
joaquin orozco <joaquincuervito8@gmail.com>
2024-05-30 16:58:28 -0300

    trabajo

---
+++ trabajo-menu/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer2
+{
+    class Menu
+    {
+        public string[] items;
+        public string nombreMenu;
+        public int posMenu;
+
+        public Menu(int posMenu, string nombreMenu, string[] opciones)
+        {
+            this.items = opciones;
+            this.nombreMenu = nombreMenu;
+            this.posMenu = posMenu;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menu1 = { "Nuevo ", "Abrir", "Clonar Repositorio" };
+            string[] menu2 = { "Ir a", "Buscar y Reemplazar", "Ir a base" };
+            string[] menu3 = { "Codigo", "Abrir", "Abrir con" };
+
+            var menus = new Dictionary<string, string[]>
+            {
+                { "Archivo", menu1 }, { "Editar", menu2 }, { "Ver", menu3}
+            };
+
+            MenuPrincipal menu = new MenuPrincipal(menus);
+            menu.Navegar();
+        }
+    }
+}
